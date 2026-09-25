750c7b6905095302dde5f5301f6eac208caab169
GeoArkadeep <arkadeep_ghosh@rocklab.in>
2026-09-26 01:27:06 +0530

    new flags get ready to roll back

--- install.sh
+++ install.sh
@@ -172,7 +172,8 @@ EOF
         DOWNLOAD_SUCCESS=true
         
         export ANDROID_API_LEVEL=24
-
+        export LDFLAGS="-lpython3.13"
+        export RUSTFLAGS="-C target-feature=+crt-static -C link-arg=-Wl,--allow-shlib-undefined"
         if [ "$DOWNLOAD_SUCCESS" = true ]; then
             echo "[*] Installing FEDERaiDE with full extras [all] using pre-compiled wheels on Python 3.13..."
 
@@ -185,7 +186,6 @@ EOF
                 --with weasyprint \
                 --with cffi \
                 --with cryptography \
-                --with mcp \
                 "federaide"
         else
             echo "[!] Pre-compiled wheels not found."
@@ -201,7 +201,6 @@ EOF
                 --with weasyprint \
                 --with cffi \
                 --with cryptography \
-                --with mcp \
                 federaide
         fi
         
